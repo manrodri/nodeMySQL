99a1f0084b7cb3096c4f1e2eeeab5270cf6d036d
Cloud User <ec2-user@ip-192-168-1-13.eu-west-1.compute.internal>
2019-06-25 13:40:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var port = 8081;
+var mysql = require('mysql');
+
+
+
+var connection = mysql.createConnection({
+  host: '192.168.3.12',
+  user: 'root',
+  password: 'password',
+  database: 'test'
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+];
+    
+app.get("/", function(req, res){
+    
+	connection.connect();
+
+   connection.query("SELECT name FROM employees WHERE location='Australia'", function (err, rows, fields) {
+      if (err) throw err
+
+   var name = ('The solution is: ', rows[0].name);
+    res.render("landing", {name: name});
+   connection.end();
+});
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.listen(port, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
